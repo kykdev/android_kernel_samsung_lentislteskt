146d182f6f4bb1f87f94b566e0e3ac7f7a2375ea
kykdev <kykint@naver.com>
2016-05-24 23:36:51 +0900

    fix compilation again
    Signed-off-by: kykdev <kykint@naver.com>

--- arch/arm/mach-msm/sec_debug.c
+++ arch/arm/mach-msm/sec_debug.c
@@ -1941,8 +1941,6 @@ int sec_debug_subsys_init(void)
 		&secdbg_krait->log.next_idx_paddr,
 		&secdbg_krait->log.log_paddr, &secdbg_krait->log.size);
 		
-	sec_debug_subsys_set_logger_info(&secdbg_krait->logger_log);
-
 	secdbg_krait->tz_core_dump =
 		(struct tzbsp_dump_buf_s **)get_wdog_regsave_paddr();
 #if 0 //MSM8974 doesn't use the msm_fb.c. and JBP doesn't use fb on kernel side.
